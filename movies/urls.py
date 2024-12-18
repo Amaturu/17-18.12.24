217e526a2db81571fb9144c4eb4d0c1fe3ee5d5e
Amaturu <hrthefreeboy@gmail.com>
2024-12-18 14:53:23 +0300

    Final Result

--- movies/urls.py
+++ movies/urls.py
@@ -1,19 +1,11 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from movies import views
 
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('movies/',
-        views.MovieList.as_view(),
-        name='movie-list'),
-    path('movies/<int:pk>/',
-        views.MovieDetail.as_view(),
-        name='movie-detail'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
-])
+router = DefaultRouter()
+router.register(r'movies', views.MovieViewSet, basename='movie')
+router.register(r'users', views.UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- movies/views.py
+++ movies/views.py
@@ -1,37 +1,23 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 from movies.serializers import UserSerializer
 from movies.permissions import IsOwnerOrReadOnly
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'movies': reverse('snippet-list', request=request, format=format)
-    })
-    
-class MovieList(generics.ListCreateAPIView):
+
+class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True)
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
