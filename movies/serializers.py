8c230d89b1c8e6c88b49feb3e892c7acb701557e
Amaturu <hrthefreeboy@gmail.com>
2024-12-18 14:30:48 +0300

    Work with fifth tutorial

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,15 +2,15 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Movie
-        fields = ['id', 'owner', 'title', 'description', 'release_date', 'rating'] 
+        fields = ['url', 'id', 'owner', 'title', 'description', 'release_date', 'rating'] 
 
-class UserSerializer(serializers.ModelSerializer):
-    movies = serializers.PrimaryKeyRelatedField(many=True, queryset=Movie.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    movies = serializers.HyperlinkedRelatedField(many=True, view_name='movie-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'movies']
+        fields = ['url', 'id', 'username', 'movies']
--- movies/urls.py
+++ movies/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from movies import views
 
-urlpatterns = [
-    path('movies/', views.MovieList.as_view()),
-    path('movies/<int:pk>/', views.MovieDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-]
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('movies/',
+        views.MovieList.as_view(),
+        name='movie-list'),
+    path('movies/<int:pk>/',
+        views.MovieDetail.as_view(),
+        name='movie-detail'),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
